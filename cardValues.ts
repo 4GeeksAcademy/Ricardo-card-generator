900688cae88a14c6d97146d5f9dbf21b0fb069c7
Ricardo <rick_fm_95@hotmail.com>
2025-06-14 03:59:53 +0000

    :sparkles: Card Generator

---
+++ cardValues.ts
@@ -0,0 +1,15 @@
+export const cardValues = [
+  'A', // As
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J', // Jack
+  'Q', // Queen
+  'K'  // King
+];
--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ];
+  let icons = ["♦", "♥", "♠", "♣"];
+
+  let numberGenerator = numbers[Math.floor(Math.random()*numbers.length)];
+  let iconGenerator = icons[Math.floor(Math.random()*icons.length)];
+
+  let centerNumber = document.querySelector("#letter");
+  let spadeUp = document.querySelector("#spadeA");
+  let spadedown = document.querySelector("#spadeB");
+
+  if (iconGenerator === "♦" || iconGenerator === "♥"){
+    spadeUp.style.color = "Red"
+    spadedown.style.color = "Red"
+  };
+ 
+  centerNumber.innerText = numberGenerator;
+  spadeUp.innerText = iconGenerator;
+  spadedown.innerText = iconGenerator;
 };
